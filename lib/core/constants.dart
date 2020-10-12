2e4b7cdebe0bbc04cc39c4b8e67e0925ddf60843
isaccanedo <isaccanedo@gmail.com>
2020-10-12 15:40:02 -0300

    Initial commit

---
+++ lib/core/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const Color PLAYER1_COLOR = Colors.red;
+const Color PLAYER2_COLOR = Colors.blue;
+
+const String PLAYER1_SYMBOL = 'X';
+const String PLAYER2_SYMBOL = 'O';
+
+const String GAME_TITLE = 'Tic Tac Toe';
+const String TIED_TITLE = 'Jogo Empatado!';
+const String WIN_TITLE = 'Jogador [SYMBOL] venceu!';
+const String DIALOG_MESSAGE = 'Pressione o botão de reset para começar novamente!';
+const String SINGLE_PLAYER_MODE_LABEL = 'Jogador sozinho';
+const String MULTIPLAYER_MODE_LABEL = 'Dois Jogadores';
+const String RESET_BUTTON_LABEL = 'RESET';
